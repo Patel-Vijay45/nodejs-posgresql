e197eb6fb9a13b3615a4dea7c7c7ec2f78530290
vijay.codeflix@gmail.com <vijay.codeflix@gmail.com>
2024-07-10 21:25:09 +0530

    db

--- config.js
+++ config.js
@@ -1,12 +1,13 @@
 const env = process.env;
 
 const config = {
-  db: { /* do not put password or any sensitive info here, done only for demo */
-    host: env.DB_HOST || 'otto.db.elephantsql.com',
-    port: env.DB_PORT || '5432',
-    user: env.DB_USER || 'cklijfef',
-    password: env.DB_PASSWORD || 'V1qidES5k3DSJICDRgXtyT8qeu2SPCZp',
-    database: env.DB_NAME || 'cklijfef',
+  db: {
+    /* do not put password or any sensitive info here, done only for demo */
+    host: env.POSTGRES_HOST,
+    port: env.DB_PORT || "5432",
+    user: env.POSTGRES_USER,
+    password: env.POSTGRES_PASSWORD,
+    database: env.POSTGRES_DATABASE,
   },
   listPerPage: env.LIST_PER_PAGE || 10,
 };
