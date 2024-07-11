61984fb760de80f620a3008a5d9bcf2098da3088
vijay.codeflix@gmail.com <vijay.codeflix@gmail.com>
2024-07-11 17:27:15 +0530

    wip

--- services/db.js
+++ services/db.js
@@ -1,20 +1,23 @@
-const { Pool } = require('pg');
-const config = require('../config');
-const pool = new Pool(config.db);
+const { Pool } = require("pg");
+const config = require("../config");
+const pool = new Pool({
+  connectionString:
+    "postgres://default:TFtbpzsSH0C9@ep-nameless-frog-a4uj3j7m.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require",
+});
 
 /**
  * Query the database using the pool
- * @param {*} query 
- * @param {*} params 
- * 
+ * @param {*} query
+ * @param {*} params
+ *
  * @see https://node-postgres.com/features/pooling#single-query
  */
 async function query(query, params) {
-    const {rows, fields} = await pool.query(query, params);
+  const { rows, fields } = await pool.query(query, params);
 
-    return rows;
+  return rows;
 }
 
 module.exports = {
-  query
-}
+  query,
+};
