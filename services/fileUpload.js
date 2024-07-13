ab6e715e0e21548ad45a9761b1dfd5e5a8a4d767
vijay.codeflix@gmail.com <vijay.codeflix@gmail.com>
2024-07-13 18:37:26 +0530

    wip

--- services/fileUpload.js
+++ services/fileUpload.js
@@ -1,9 +1,9 @@
 const multer = require("multer");
-const path = require('path');
+const path = require("path");
 // Set up storage for uploaded files
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "../uploads/");
+    cb(null, path.join(__dirname + "/uploads/"));
   },
   filename: (req, file, cb) => {
     cb(null, Date.now() + "-" + file.originalname);
@@ -22,9 +22,10 @@ const upload = multer({
 function checkFileType(file, cb) {
   const filetypes = /xls|xlsx/;
   const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-  const mimetype = file.mimetype === 'application/vnd.ms-excel' || 
-  file.mimetype === 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet';
-
+  const mimetype =
+    file.mimetype === "application/vnd.ms-excel" ||
+    file.mimetype ===
+      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
 
   if (extname && mimetype) {
     return cb(null, true);
